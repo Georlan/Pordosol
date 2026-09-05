0b8d1e03d14e2b44e1f242d290263a18d1c2304f
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-05 12:54:40 -0300

    feat: redesenhar header profissional
    Fase 01: novo header com logotipo oficial tratado, navegação refinada, CTA comercial e responsividade dedicada.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './patch.css'
 import './modern.css'
+import './header-refresh.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
