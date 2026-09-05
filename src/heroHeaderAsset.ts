705c5f0d27658ccaf159dcf0eff30ebc0cdb552d
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-05 12:17:41 -0300

    feat: montar asset da nova hero

---
+++ src/heroHeaderAsset.ts
@@ -0,0 +1,15 @@
+import part00 from './heroAssetChunks/part00'
+import part01 from './heroAssetChunks/part01'
+import part02 from './heroAssetChunks/part02'
+import part03 from './heroAssetChunks/part03'
+import part04 from './heroAssetChunks/part04'
+import part05 from './heroAssetChunks/part05'
+import part06 from './heroAssetChunks/part06'
+import part07 from './heroAssetChunks/part07'
+import part08 from './heroAssetChunks/part08'
+import part09 from './heroAssetChunks/part09'
+import part10 from './heroAssetChunks/part10'
+import part11 from './heroAssetChunks/part11'
+
+export const heroHeaderAsset =
+  `data:image/webp;base64,${part00}${part01}${part02}${part03}${part04}${part05}${part06}${part07}${part08}${part09}${part10}${part11}`
